8070756a730b338a6c76764d0315fbdd2409e874
max <maxtwinslow1@gmail.com>
2024-04-13 12:59:09 -0700

    x

--- content.js
+++ content.js
@@ -1,18 +1,20 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     
-    if (request.id == "NEW_TICKET") {
-        const labels = document.querySelector('.ticket_fields.with_omnipanel').querySelectorAll('label');
-        console.log(labels)
-        for (let i = 0; i < labels.length; i++) {
-          let label = labels[i]
-          if (label.textContent.trim().endsWith('*')) {
-            label.style.backgroundColor = 'red';
-            label.style.color = 'white';
-          }
-          else {
-            label.style.display = 'none';
-          }
-        }
+if (request.id == "NEW_TICKET") {
+  var emberViews = document.querySelectorAll('.ember-view');
+
+  for (let i = 0; i < emberViews.length; i++) {
+    var labels = emberViews[i].querySelectorAll('label');
+    for (let j = 0; j < labels.length; j++) {
+      console.log(labels[j].textContent)
+      if (labels[j].textContent.trim().endsWith('*')) {
+        labels[j].style.backgroundColor = '';
+        labels[j].style.backgroundColor = 'red';
+        labels[j].style.color = '';
+        labels[j].style.color = 'white';
+      }
     }
+  }
+}
 });
