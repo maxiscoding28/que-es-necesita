85971e71b89cf57ff5041fd7cf654e9217968be9
max <maxtwinslow1@gmail.com>
2024-04-13 13:07:23 -0700

    x

--- content.js
+++ content.js
@@ -1,20 +1,22 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    
-if (request.id == "NEW_TICKET") {
-  var emberViews = document.querySelectorAll('.ember-view');
+function updateStyles(request) {
+  if (request.id == "NEW_TICKET") {
+    var emberViews = document.querySelectorAll('.ember-view');
 
-  for (let i = 0; i < emberViews.length; i++) {
-    var labels = emberViews[i].querySelectorAll('label');
-    for (let j = 0; j < labels.length; j++) {
-      console.log(labels[j].textContent)
-      if (labels[j].textContent.trim().endsWith('*')) {
-        labels[j].style.backgroundColor = '';
-        labels[j].style.backgroundColor = 'red';
-        labels[j].style.color = '';
-        labels[j].style.color = 'white';
+    for (let i = 0; i < emberViews.length; i++) {
+      var labels = emberViews[i].querySelectorAll('label');
+      for (let j = 0; j < labels.length; j++) {
+        if (labels[j].textContent.trim().endsWith('*')) {
+          labels[j].style.backgroundColor = '';
+          labels[j].style.backgroundColor = 'red';
+          labels[j].style.color = '';
+          labels[j].style.color = 'white';
+        }
       }
     }
   }
 }
+
+chrome.runtime.onMessage.addListener(
+  function(request) {
+    updateStyles(request)
 });
